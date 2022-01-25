bf94ae96fd5ef427f5f59701f4e3208eba2a3877
Sergej Venhura <71256928+venguraserg@users.noreply.github.com>
2022-01-25 17:08:49 +0300

    Создана структурa MVVM

---
+++ HW13_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HW13_WPF.ViewModels.Base;
+
+namespace HW13_WPF.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
