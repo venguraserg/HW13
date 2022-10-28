bba36ed1b62258fae49ca26628eb9213e357fdd6
Венгура Сергей <venguraserg@mail.ru>
2022-10-28 23:55:01 +0300

    Создана базовая инфраструктура MVVM

---
+++ HW13WPF/Model/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HW13WPF.Model
+{
+    public class Client : INotifyPropertyChanged
+    {
+        private Guid id;
+        private string name;
+        private string surName;
+
+        public Guid Id
+        {
+            get => id;
+            set
+            {
+                id = value; 
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string SurName
+        {
+            get => surName;
+            set
+            {
+                surName = value;
+                OnPropertyChanged("SurName");
+            }
+        }
+
+
+        public Client(string name, string surName)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            SurName = surName;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ HW13WPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using HW13WPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HW13WPF.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Client> clients;
+        public ObservableCollection<Client> Clients
+        {
+            get => clients;
+            set
+            {
+                clients = value;
+                OnPropertyChanged("Clients");
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            Clients = new ObservableCollection<Client>();
+
+            for(int i = 0; i < 100; i++)
+            {
+                Clients.Add(new Client("Name" + i.ToString(), "Surname" + i.ToString()));
+            }
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
