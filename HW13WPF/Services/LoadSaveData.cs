4a212792ce4c63ddfef3bf4b8acda46d40ec286a
Венгура Сергей <venguraserg@mail.ru>
2022-10-30 00:51:37 +0300

    Автозаполгнение, Серилизация, десерилизация

---
+++ HW13WPF/Services/LoadSaveData.cs
@@ -0,0 +1,73 @@
+﻿using HW13WPF.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace HW13WPF.Services
+{
+    public static class LoadSaveData
+    {
+        
+        /// <summary>
+        /// Автозаполнение клиентов для теста
+        /// </summary>
+        /// <param name="number"></param>
+        public static ObservableCollection<Client> ClientAutofill(int number , string FILE_NAME)
+        {
+            var Clients = new ObservableCollection<Client>();
+            for (int i = 0; i < number; i++)
+            {
+                string tempGuid = Guid.NewGuid().ToString();
+                string[] stringMassive = tempGuid.Split(new char[] { '-' });
+
+                Clients.Add(new Client(stringMassive[0], stringMassive[1])); ;
+            }
+            Save(FILE_NAME, Clients);
+            return Clients;
+        }
+        /// <summary>
+        /// Десериализация данных их файла
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static ObservableCollection<T> Load<T>(string fileName)
+        {
+            ObservableCollection<T> tempItems = new ObservableCollection<T>();
+
+            if (File.Exists(fileName) == false)
+            {
+                using (File.Create(fileName)) { };
+                ObservableCollection<T> tempItemList = new ObservableCollection<T>();
+                return tempItemList;
+            }
+            else
+            {
+                string json = File.ReadAllText(fileName);
+                if (string.IsNullOrEmpty(json)) return new ObservableCollection<T>();
+                var items = JsonConvert.DeserializeObject<ObservableCollection<T>>(json);
+                return items;
+            }
+        }
+
+        /// <summary>
+        /// Сериализация данных их файла
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <param name="items"></param>
+        public static void Save<T>(string fileName, ObservableCollection<T> items)
+        {
+            if (File.Exists(fileName) == false)
+            {
+                using (File.Create(fileName)) { };
+            }
+
+            string json = JsonConvert.SerializeObject(items);
+            File.WriteAllText(fileName, json);
+        }
+    }
+}
--- HW13WPF/ViewModel/MainWindowViewModel.cs
+++ HW13WPF/ViewModel/MainWindowViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using HW13WPF.Model;
+using HW13WPF.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows;
 
 namespace HW13WPF.ViewModel
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private const string FILE_NAME = "data.json";
+
         private ObservableCollection<Client> clients;
+        private Client selectedClient;
         public ObservableCollection<Client> Clients
         {
             get => clients;
@@ -20,15 +25,29 @@ namespace HW13WPF.ViewModel
                 OnPropertyChanged("Clients");
             }
         }
+        public Client SelectedClient
+        {
+            get => selectedClient;
+            set
+            {
+                selectedClient = value;
+                OnPropertyChanged("SelectedClient");
+            }
+        }
+
 
 
         public MainWindowViewModel()
         {
             Clients = new ObservableCollection<Client>();
 
-            for(int i = 0; i < 100; i++)
+            Clients = LoadSaveData.Load<Client>(FILE_NAME);
+            if(Clients.Count == 0)
             {
-                Clients.Add(new Client("Name" + i.ToString(), "Surname" + i.ToString()));
+                if(MessageBox.Show("Список клиентов пуст\nЗаполнить Автоматически?", "Информация", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+                    Clients = LoadSaveData.ClientAutofill(100, FILE_NAME);
+                }
             }
         }
 
